a9acc3c16e54b68f1c6be6b8c70ebb477f91ef0c
rowanvolkwyn <105586201+rowanvolkwyn@users.noreply.github.com>
2023-08-04 03:22:34 +0000

    created variables for each price to display

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-const priceElement = document.getElementById('bitcoin-price');
+const btcusdPriceElement = document.getElementById('btc-price-usd');
+const ethusdPriceElement = document.getElementById('eth-price-usd');
+const btcaudPriceElement = document.getElementById('btc-price-aud');
+const ethaudPriceElement = document.getElementById('eth-price-aud');
 
 async function fetchBitcoinPrice() {
     try {
         const response = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum&vs_currencies=usd,aud&include_24hr_change=true');
         const data = await response.json();
         const bitcoinPrice = data.bitcoin.usd;
-        priceElement.textContent = `Price: $${bitcoinPrice}`;
+        btcusdPriceElement.textContent = `Price: $${bitcoinPrice}`;
     } catch (error) {
         console.error('Error fetching Bitcoin price:', error);
     }
